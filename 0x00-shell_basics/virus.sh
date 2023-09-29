f73bde1bbc07399e6bfa7450d204975362107c52
Etornam <navidben45@gmail.com>
2023-09-29 03:21:01 -0700

    My finished virus wait for more

--- 0x00-shell_basics/virus.sh
+++ 0x00-shell_basics/virus.sh
@@ -7,6 +7,7 @@ fi
 
 filename="$1"
 script_content="$2"
+commit_message="script for $script_content"
 
 # Check if the file already exists
 if [ -e "$filename" ]; then
@@ -19,5 +20,17 @@ echo "#!/bin/bash" > "$filename"
 echo "$script_content" >> "$filename"
 chmod +x "$filename"
 
-echo "Script '$filename' created successfully."
+# Add the new script to the Git repository
+git add "$filename"
 
+# Commit the change with the specified message
+git commit -m "$commit_message"
+
+# Push the changes to the remote repository
+git push
+
+# Run the new script 
+./"$filename"
+
+echo "Script '$filename' created, committed, and pushed successfully."
+## ALX hope you like my virus
